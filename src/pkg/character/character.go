fc7b4926f61cdad5ff08ca55f2292d09cec728cb
ducc <aaronhwang22@gmail.com>
2024-12-27 20:34:43 -0500

    Add character roles, add ult chain assists

--- src/pkg/character/character.go
+++ src/pkg/character/character.go
@@ -2,7 +2,13 @@ package character
 
 import "zzz-optimizer/pkg/model"
 
-var ()
+var (
+	CharacterCatalog map[string]CharacterConfig
+)
+
+func Register(key string, config CharacterConfig) {
+	CharacterCatalog[key] = config
+}
 
 type CharacterRarity int
 
@@ -11,13 +17,26 @@ const (
 	SRank CharacterRarity = 5
 )
 
+type CharacterRole int
+
+const (
+	Role_INVALID CharacterRole = -1
+	Role_ATTACK  CharacterRole = 0
+	Role_ANOMALY CharacterRole = 1
+	Role_SUPPORT CharacterRole = 2
+	Role_DEFENSE CharacterRole = 3
+	Role_STUN    CharacterRole = 4
+)
+
 type CharacterConfig struct {
 	Rarity     CharacterRarity
 	Promotions []PromotionData
 	// Probably make an enum for this at some point
 	Faction string
 	// Ditto
-	Element string
+	Element   string
+	Role      CharacterRole
+	SkillInfo SkillInfo
 }
 
 // Characters all have the following (Promotion related) data
@@ -33,14 +52,44 @@ type PromotionData struct {
 }
 
 type SkillInfo struct {
-	BasicAtk BasicAttack
+	BasicAtk    BasicAttack
+	Special     Special
+	DodgeAttack DodgeAttack
+	Assist      Assist
+	Ult         Ult
+	ChainAttack ChainAttack
 }
 
 type BasicAttack struct {
 	TargetType model.TargetType
 }
 
-type Skill struct {
+type DodgeAttack struct {
+	TargetType model.TargetType
+}
+
+type Special struct {
 	Cost       int
+	TargetType model.TargetType
+}
+
+type Ult struct {
+	TargetType model.TargetType
+}
+
+type ChainAttack struct {
+	TargetType model.TargetType
+}
+
+type AssistType int
+
+const (
+	AssistType_NULL      AssistType = 0
+	ASsistType_DEFENSIVE AssistType = 1
+	AssistType_EVASIVE   AssistType = 2
+)
+
+type Assist struct {
+	AssistType AssistType
 	TargetType model.TargetType
 }
