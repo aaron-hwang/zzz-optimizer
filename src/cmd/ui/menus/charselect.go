6fd5c0993ee5ba74a15e0e38d9425f8e6932f794
ducc <aaronhwang22@gmail.com>
2025-01-02 22:31:41 -0500

    Implemented character select menu to have dialog for selecting new characters to add. properly handles killing the dialog on button press.

--- src/cmd/ui/menus/charselect.go
+++ src/cmd/ui/menus/charselect.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"errors"
 	"zzz-optimizer/pkg/character"
 
 	"fyne.io/fyne/v2"
@@ -11,10 +12,32 @@ import (
 
 func charSelect(w fyne.Window) fyne.CanvasObject {
 	content := container.NewVBox()
-	for key := range character.CharacterCatalog {
+	content.Refresh()
+	content.Add(widget.NewButton("Add character", func() {
+		charCatalog(w)
+	}))
+	for key, char := range Optimizer.Characters {
 		content.Add(widget.NewButton(string(key), func() {
-			dialog.ShowCustom(string(key), "x", charView(key), w)
+			dialog.ShowCustom(string(key), "x", charView(char), w)
 		}))
 	}
 	return content
+}
+
+// Handles the logic of showing the character catalog.
+func charCatalog(w fyne.Window) {
+	content := container.NewVBox()
+	dlg := dialog.NewCustom("Characters", "exit", content, w)
+	for key := range character.CharacterCatalog {
+		content.Add(widget.NewButton(string(key), func() {
+			if _, ok := Optimizer.Characters[key]; ok {
+				d := dialog.NewError(errors.New("character already owned"), w)
+				d.Show()
+			} else {
+				Optimizer.Characters[key] = character.NewLevelZeroChar(key)
+				dlg.Hide()
+			}
+		}))
+	}
+	dlg.Show()
 }
