af2c8942331714f7c3360a11464962a7238b0421
ducc <aaronhwang22@gmail.com>
2025-01-02 22:28:32 -0500

    implemented character view for an owned character.

--- src/cmd/ui/menus/characterview.go
+++ src/cmd/ui/menus/characterview.go
@@ -1,17 +1,18 @@
 package ui
 
 import (
-	"zzz-optimizer/pkg/key"
+	"strconv"
+	"zzz-optimizer/pkg/character"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
-// This page should handle the display of the user's current state of their characters.
-func charView(char key.Character) fyne.CanvasObject {
+// This page should handle the display of the user's current state of their given character.
+func charView(char character.Character) fyne.CanvasObject {
 	content := container.NewVBox()
-	character := Optimizer.Characters[char]
-	content.Add(widget.NewLabel(string(character.Key)))
+	content.Add(widget.NewLabel(string(char.Key)))
+	content.Add(widget.NewLabel("Core Level: " + strconv.Itoa(char.SkillLevels.Core)))
 	return content
 }
